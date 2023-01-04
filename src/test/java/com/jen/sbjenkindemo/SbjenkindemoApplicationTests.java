8547d3c31c52af52a6611d5b54829b01d6de9937
Mani <manikumar.panithi@gmail.com>
2023-01-04 21:11:51 +0530

    first commit

---
+++ src/test/java/com/jen/sbjenkindemo/SbjenkindemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jen.sbjenkindemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbjenkindemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
